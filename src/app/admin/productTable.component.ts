3ebfac4939cbddb1d7748a5287ed013462f6627c
anu <anujp.jp@gmail.com>
2020-05-07 18:01:23 -0700

    admin

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,5 +1,18 @@
-import {Component} from '@angular/core'
+import { Component } from '@angular/core'
+import { Product } from '../model/product.model'
+import { ProductRepository } from '../model/product.repository'
+
 @Component({
-    template:'<h3>Product Table</h3>',
+  templateUrl: 'productTable.component.html',
 })
-export class ProductTable {}
+export class ProductTableComponent {
+  constructor(private repository: ProductRepository) {}
+
+  getProducts(): Product[] {
+    return this.repository.getProducts()
+  }
+
+  deleteProduct(id: number) {
+    this.repository.deleteProduct(id)
+  }
+}
