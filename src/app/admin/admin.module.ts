2c07d49c024e4204c9faa2d28a9997c86ef34d83
anu <anujp.jp@gmail.com>
2020-05-07 17:03:50 -0700

    model module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,10 +4,11 @@ import {AdminComponent} from './admin.component';
 import{CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import{NgModule} from '@angular/core';
+import { AuthGuard } from './auth.guard';
 
 const routing = RouterModule.forChild([
     { path: 'auth', component: AuthComponent },
-    { path: 'main', component: AdminComponent },
+    { path: 'main', component: AdminComponent ,canActivate :[AuthGuard]},
     { path: '**', redirectTo: 'auth' },
   ])
   
@@ -15,6 +16,7 @@ const routing = RouterModule.forChild([
   @NgModule({
     imports: [CommonModule, FormsModule, routing],
     declarations: [AuthComponent, AdminComponent],
+    providers: [AuthGuard],
   
   })
   export class AdminModule{}
